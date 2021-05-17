133433a74ad48a327561ef2cc513dd954121e771
ktny <katsunori.t10@gmail.com>
2021-05-17 21:58:21 +0900

    make components dir

---
+++ components/layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import styles from "../styles/layout.module.css";
+
+export const siteName = "ブクマンガ";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="はてなブックマーク数を元にweb漫画をまとめているサイトです。" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteName
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <title>{siteName}</title>
+        <meta name="og:title" content={siteName} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>{siteName}</header>
+      <main>{children}</main>
+      <footer>bukumanga.com 2021</footer>
+    </div>
+  );
+}
