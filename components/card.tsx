6d39dad9f9c5b717bf23b85f975e234ddee31d92
ktny <katsunori.t10@gmail.com>
2021-05-19 22:50:21 +0900

    add design

--- components/card.tsx
+++ components/card.tsx
@@ -1,20 +1,36 @@
+import React from "react";
 import Image from "next/image";
 import styles from "../styles/card.module.scss";
 
 export default function Card({ entry }: { entry: any }) {
+  const clickCard = (e: React.MouseEvent) => {
+    window.open(entry.url, "_blank");
+  };
+  const clickBookMark = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    window.open(`https://b.hatena.ne.jp/entry/s/${entry.url.replace("https://", "")}`, "_blank");
+  };
+
+  const dummyImg =
+    "https://cdn-ak-scissors.b.st-hatena.com/image/square/abf4f339344e96f39ffb9c18856eca5d454e63f8/height=280;version=1;width=400/https%3A%2F%2Fanond.hatelabo.jp%2Fimages%2Fog-image-1500.gif";
+
   return (
-    <div className={styles.card}>
+    <section className={styles.card} onClick={clickCard}>
       <div className={styles.bookmarkCount}>
-        <span className={styles.bookmarkCountNum}>B! {entry.bookmark_count}</span>
+        <div className={styles.bookmarkCountNum} onClick={clickBookMark}>
+          B! {entry.bookmark_count}
+        </div>
       </div>
+      <Image src={entry.image.Valid ? entry.image.String : dummyImg} alt={entry.title} width="300" height="210"></Image>
       <div className={styles.body}>
         <div className={styles.titleWrapper}>
-          <div className={styles.title}>{entry.title}</div>
+          <div className={styles.title} title={entry.title}>
+            {entry.title}
+          </div>
         </div>
-        <div className={styles.img}>
-          <Image src={entry.image.String} alt={entry.title} width="256" height="179"></Image>
-        </div>
+        <div className={styles.domain}>{entry.domain}</div>
       </div>
-    </div>
+    </section>
   );
 }
