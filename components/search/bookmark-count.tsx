4e46d35be17d03d47f0cc5b60c2a99a48a9ad965
ktny <katsunori.t10@gmail.com>
2021-06-12 21:46:55 +0900

    add use-media

--- components/search/bookmark-count.tsx
+++ components/search/bookmark-count.tsx
@@ -1,30 +1,22 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { Grid, Input, Slider, Typography } from "@material-ui/core";
-
-const marks = [
-  { value: 0, label: "0" },
-  { value: 100, label: "100" },
-  { value: 200, label: "200" },
-  { value: 300, label: "300" },
-  { value: 400, label: "400" },
-  { value: 500, label: "500" },
-  { value: 600, label: "600" },
-  { value: 700, label: "700" },
-  { value: 800, label: "800" },
-  { value: 900, label: "900" },
-  { value: 1000, label: "1000" },
-];
+import { range } from "../../helpers/util";
 
 export default function BookmarkCount({
   bookmarkCount,
   setBookmarkCount,
+  isSP,
 }: {
   bookmarkCount: number;
   setBookmarkCount: Dispatch<SetStateAction<number>>;
+  isSP: boolean;
 }) {
   const MIN = 0;
   const MAX = 1000;
   const STEP = 10;
+
+  const marksStep = isSP ? 200 : 100;
+  const marks = range(0, 1001, marksStep).map(n => ({ value: n, label: n.toString() }));
 
   const handleSliderChange = (event: any, newValue: number) => {
     setBookmarkCount(newValue);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDebounce } from "use-debounce";
+import useMedia from "use-media";
 import EntryList from "../components/entry-list";
 import Layout from "../components/layout";
 import search, { PER_PAGE } from "./api/search";
@@ -10,7 +11,7 @@ export default function Home() {
   const defaultStartDate = new Date();
   defaultStartDate.setDate(defaultStartDate.getDate() - 7); // デフォルトを今週にする
 
-  // エントリ一覧と検索条件のstate
+  // 各種state
   const [entries, setEntries] = useState([]);
   const [startDate, setStartDate] = useState(defaultStartDate);
   const [endDate, setEndDate] = useState(defaultEndDate);
@@ -22,6 +23,7 @@ export default function Home() {
   const [hasMore, setHasMore] = useState(true);
   const [count, setCount] = useState(0);
   const [isHeaderExpanded, setIsHeaderExpanded] = useState(false);
+  const isSP = useMedia({ maxWidth: "480px" });
 
   // イベントを間引くためにdebounce変数をトリガーにする
   const [debounceStartDate] = useDebounce(startDate, 500);
@@ -81,6 +83,7 @@ export default function Home() {
     setCount,
     isHeaderExpanded,
     setIsHeaderExpanded,
+    isSP,
   };
 
   return (
