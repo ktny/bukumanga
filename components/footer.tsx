2a2e0881e8698b10f87124c70653fb2bb9120a1c
ktny <katsunori.t10@gmail.com>
2021-06-13 21:51:51 +0900

    add github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,5 +2,14 @@ import classes from "../styles/footer.module.scss";
 import { siteName } from "../pages/_app";
 
 export default function Footer() {
-  return <footer className={classes.footer}>&copy; 2021 {siteName.toLowerCase()}</footer>;
+  return (
+    <footer className={classes.footer}>
+      <div>&copy; 2021 {siteName.toLowerCase()}</div>
+      <div>
+        <a href="https://github.com/ktny/bukumanga" target="_blank">
+          GitHub
+        </a>
+      </div>
+    </footer>
+  );
 }
