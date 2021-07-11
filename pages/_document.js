7595814e86b7796ae67dd37a82f1ef9ac7f2d1e7
ktny <katsunori.t10@gmail.com>
2021-07-11 16:42:57 +0900

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import theme from "../src/theme";
 import { existsGaId, GA_ID } from "../src/lib/gtag";
 
 export default class MyDocument extends Document {
@@ -12,7 +11,6 @@ export default class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link
             rel="stylesheet"
